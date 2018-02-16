c840f263faa84f63ecf1cecb73c608fa46499fb7
Jörg Breitbart <jerch@rockborn.de>
2018-02-16 06:18:19 +0100

    pypi package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'django-treewidget',
+    packages = ['django-treewidget'],
+    version = '0.1',
+    description = 'treewidget for django admin',
+    author = 'Joerg Breitbart',
+    author_email = 'j.breitbart@netzkolchose.de',
+    url = 'https://github.com/jerch/django-treewidget',
+    download_url = 'https://github.com/jerch/django-treewidget/archive/0.1.tar.gz',
+    keywords = ['django', 'widget', 'admin', 'tree'],
+    classifiers = [],
+)
+
